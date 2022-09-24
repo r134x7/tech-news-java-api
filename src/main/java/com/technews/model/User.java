f3d41c9f9fa80f640c78bb9309da22e549df1ccd
r134x7 <r134x7@gmx.com>
2022-09-24 22:48:58 +1000

    used getters for the override equals and hash codes

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -106,12 +106,12 @@ public class User implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return loggedIn == user.loggedIn && Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(posts, user.posts) && Objects.equals(votes, user.votes) && Objects.equals(comments, user.comments);
+        return isLoggedIn() == user.isLoggedIn() && Objects.equals(getId(), user.getId()) && Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getPassword(), user.getPassword()) && Objects.equals(getPosts(), user.getPosts()) && Objects.equals(getVotes(), user.getVotes()) && Objects.equals(getComments(), user.getComments());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, email, password, loggedIn, posts, votes, comments);
+        return Objects.hash(getId(), getUsername(), getEmail(), getPassword(), isLoggedIn(), getPosts(), getVotes(), getComments());
     }
 
     @Override
