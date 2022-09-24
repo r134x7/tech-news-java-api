986e0123a3589ec55c567a907e5fe5c8279c4993
r134x7 <r134x7@gmx.com>
2022-09-24 21:50:43 +1000

    added annotations to date variables

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,15 +1,33 @@
 package com.technews.model;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
 public class Post implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String title;
     private String postUrl;
+    @Transient
     private String userName;
+    @Transient
     private int voteCount;
     private Integer userId;
+    @NotNull // Date variable cannot be null
+    @Temporal(TemporalType.DATE) // to use the Date type inside the database
+    @Column(name = "posted_at") // column name for database
     private Date postedAt = new Date();
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_at")
     private Date updatedAt = new Date();
     private List<Comment> comments;
 }
