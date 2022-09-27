bd5305b3017c8d9e5f98459c55b5bbcc01a8f3fd
r134x7 <r134x7@gmx.com>
2022-09-27 18:27:12 +1000

    added login map

--- src/main/java/com/technews/controller/HomePageController.java
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -1,11 +1,16 @@
 package com.technews.controller;
 
+import com.technews.model.User;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomePageController {
@@ -21,5 +26,16 @@ public class HomePageController {
 
     @Autowired
     CommentRepository commentRepository;
+
+    @GetMapping("/login")
+    public String login(Model model, HttpServletRequest request) {
+
+        if (request.getSession(false) != null) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("user", new User());
+        return "login";
+    }
 
 }
