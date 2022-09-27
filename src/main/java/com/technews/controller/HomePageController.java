eebefdbe0b812b4d3b6d39748af339a816a05527
r134x7 <r134x7@gmx.com>
2022-09-27 18:23:54 +1000

    added homepage controller and repository objects

---
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -0,0 +1,25 @@
+package com.technews.controller;
+
+import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomePageController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+}
