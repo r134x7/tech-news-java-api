b25b66a66c80882e2ff4c9608904dc978d4eaba8
r134x7 <r134x7@gmx.com>
2022-09-27 19:08:46 +1000

    added post postmap

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -1,5 +1,6 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
 import com.technews.model.User;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
@@ -99,5 +100,21 @@ public class TechNewsController {
         return "redirect:/dashboard";
     }
 
-    
+    @PostMapping("/posts")
+    public String addPostDashboardPage(@ModelAttribute Post post, Model model, HttpServletRequest request) {
+
+        if ((post.getTitle().equals(null) || post.getTitle().isEmpty()) || (post.getPostUrl().equals(null) || post.getPostUrl().isEmpty())) {
+            return "redirect:/dashboardEmptyTitleAndLink";
+        }
+
+        if (request.getSession(false) == null) {
+            return "redirect:/login";
+        } else {
+            User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            post.setUserId(sessionUser.getId());
+            postRepository.save(post);
+
+            return "redirect:/dashboard";
+        }
+    }
 }
