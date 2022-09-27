307b54ea95e19048bcec8a5f310a9a95a20b13e3
r134x7 <r134x7@gmx.com>
2022-09-27 18:56:57 +1000

    adding postcontroller

---
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -0,0 +1,26 @@
+package com.technews.controller;
+
+import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TechNewsController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+
+}
