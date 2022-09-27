b5756601d9285dab7fb41826f285c8969a438b6d
r134x7 <r134x7@gmx.com>
2022-09-27 19:01:51 +1000

    added postmap login

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -1,11 +1,18 @@
 package com.technews.controller;
 
+import com.technews.model.User;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TechNewsController {
@@ -22,5 +29,38 @@ public class TechNewsController {
     @Autowired
     CommentRepository commentRepository;
 
+    @PostMapping("/users/login")
+    public String login(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
 
+        if ((user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
+            model.addAttribute("notice", "Email address and password must be populated in order to login!");
+            return "login";
+        }
+
+        User sessionUser = userRepository.findUserByEmail(user.getEmail());
+
+        try {
+            // If sessionUser is invalid, running .equals() will throw an error
+            if (sessionUser.equals(null)) {
+
+            }
+            // We will catch an error and notify client that email address is not recognized
+        } catch (NullPointerException e) {
+            model.addAttribute("notice", "Email address is not recognized!");
+            return "login";
+        }
+
+        // Validate Password
+        String sessionUserPassword = sessionUser.getPassword();
+        boolean isPasswordValid = BCrypt.checkpw(user.getPassword(), sessionUserPassword);
+        if(isPasswordValid == false) {
+            model.addAttribute("notice", "Password is not valid!");
+            return "login";
+        }
+
+        sessionUser.setLoggedIn(true);
+        request.getSession().setAttribute("SESSION_USER", sessionUser);
+
+        return "redirect:/dashboard";
+    }
 }
